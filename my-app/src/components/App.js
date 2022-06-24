63107847e18a10ec2514ee29136ec8f476ff9292
Sadaqat Ali <entre@MacBook-Pro.local>
2022-06-24 15:25:08 +1000

    added contact card, functions to add and remove contact from the list

--- my-app/src/components/App.js
+++ my-app/src/components/App.js
@@ -1,28 +1,46 @@
 
+import React, { useState, useEffect } from "react";
 import Header from './Header';
 import AddContact from './AddContact';
 import ContactList from './ContactList';
 import './App.css';
 
 function App() {
+ const LOCAL_STORAGE_KEY = 'contacts'; 
+ let randomNumber = Math.random() * 10;
+ const [contacts, setContacts] = useState([]);
+ 
+ const addContactHandler = (contact) => {
+   setContacts([...contacts, {id: randomNumber, ...contact}]);
+ };
 
-  const contacts = [
-    {
-      id: '1',
-      name: "Umer Farooq",
-      email: 'example@gmail.com'
-    },
-    {
-      id: '2',
-      name: "Asif ali",
-      email: 'asif@gmail.com'
-    }
-  ]
+ //delete contact 
+ const removeContactHandler = (id) => {
+ const newContactList = contacts.filter((contact) => {
+   return contact.id !== id;
+ });
+
+  setContacts(newContactList);
+
+ }
+
+ //retrieve data from the local storage
+ useEffect(() => {
+  const retriveContact = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+  if(retriveContact) setContacts(retriveContact);
+  console.log(contacts);
+}, []);
+
+ //add dependency for making changes 
+ useEffect(() => {
+  localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(contacts));
+ }, [contacts]);
+
   return (
     <div className='ui container'>
     <Header />
-    <AddContact />
-    <ContactList contacts = {contacts} />
+    <AddContact  addContactHandler={addContactHandler}/>
+    <ContactList contacts = {contacts} getContactId = {removeContactHandler} />
     </div>
   );
 }
--- my-app/src/components/ContactList.js
+++ my-app/src/components/ContactList.js
@@ -1,20 +1,22 @@
 import React from 'react';
+import ContactCard from './ContactCard';
 
 const ContactList = (props) => {
-    console.log(props.contacts);
+
+    const deleteContactHandler = (id) => {
+        props.getContactId(id);
+    }
 
     const renderContactList = props.contacts.map((contact) => { 
         return (
-            <div className='item'>
-                <div className='content'>
-                    <div className='header'>{contact.name}</div>
-                        <div>{contact.email}</div>
-                        <i className='trash alternate outline icon'></i>
-                </div>
-            </div>
+            <ContactCard 
+             contact={contact} 
+             clickHandler={deleteContactHandler} 
+             key={contact.id}>  
+             </ContactCard>
      );
-  })
-    // return <div className='ui celled list'>Contact List</div>;
+  });
+    return <div className='ui celled list'>{renderContactList}</div>;
 }
 
-export default ContactList;
+export default ContactList; 
